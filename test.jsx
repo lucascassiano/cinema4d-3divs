b58299c4bbec79b909c6a5d6df4636faf65baa13
Lucas Cassiano Silva <cassiano@mit.edu>
2018-03-25 15:02:47 -0400

    more tests

--- test.jsx
+++ test.jsx
@@ -6,9 +6,49 @@ import {SimpleViewer, div3D} from "react_3divs";
 class test extends Component {
     render(){
         <SimpleViewer>
+			<div3d name="Plane" 
+					width={40000.0} height={0.0}
+					position={{x:-93.6288733306,y:-460.720759748,z:-185.042985987}}
+					rotation={{x:0.0,y:0.0,z:0.0}}>
+
+				add your HTML/JSX code here 
+
+			</div3d>
+			<div3d name="div.2" 
+					width={360.445389849} height={0.0}
+					position={{x:-5.69256109231,y:309.519251866,z:175.738267705}}
+					rotation={{x:0.0,y:1.57079632679,z:0.0}}>
+
+				add your HTML/JSX code here 
+
+			</div3d>
+			<div3d name="div.1" 
+					width={110.516422197} height={0.0}
+					position={{x:67.5907051131,y:585.802170788,z:75.1546118074}}
+					rotation={{x:0.0,y:1.57079632679,z:0.0}}>
+
+				add your HTML/JSX code here 
+
+			</div3d>
+			<div3d name="0.2" 
+					width={1172.10775406} height={0.0}
+					position={{x:-550.040051747,y:471.962412914,z:-210.789280181}}
+					rotation={{x:0.0,y:1.57079632679,z:0.726191569064}}>
+
+				add your HTML/JSX code here 
+
+			</div3d>
+			<div3d name="0.1" 
+					width={531.618295271} height={0.0}
+					position={{x:-329.07953734,y:91.2592221692,z:-459.595635831}}
+					rotation={{x:0.0,y:1.57079632679,z:0.726191569064}}>
+
+				add your HTML/JSX code here 
+
+			</div3d>
 			<div3d name="something" 
 					width={179.153473785} height={0.0}
-					position={{x:439.024570974,y:-110.491484208,z:99.7898296582}}
+					position={{x:345.395697643,y:-136.682607873,z:-85.2531563287}}
 					rotation={{x:0.0,y:1.57079632679,z:0.0}}>
 
 				add your HTML/JSX code here 
@@ -16,15 +56,15 @@ class test extends Component {
 			</div3d>
 			<div3d name="0" 
 					width={531.618295271} height={0.0}
-					position={{x:-235.45066401,y:345.153504792,z:-274.552649844}}
-					rotation={{x:0.0,y:1.57079632679,z:0.0}}>
+					position={{x:-741.454892483,y:318.962381127,z:-763.461122354}}
+					rotation={{x:0.0,y:1.57079632679,z:0.726191569064}}>
 
 				add your HTML/JSX code here 
 
 			</div3d>
 			<div3d name="div" 
 					width={531.618295271} height={0.0}
-					position={{x:0.0,y:-7.65120441495e-18,z:0.124953650641}}
+					position={{x:-93.6288733306,y:-26.1911236648,z:-184.918032336}}
 					rotation={{x:0.0,y:1.57079632679,z:0.0}}>
 
 				add your HTML/JSX code here 
@@ -35,3 +75,4 @@ class test extends Component {
 }
 
 export default test
+
